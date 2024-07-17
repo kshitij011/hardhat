e1619f23e5a75fd2f277ff348659115a198d5413
Kshitij Bhoite <kshitijbhoite011@gmail.com>
2024-07-17 20:39:18 +0530

    changing configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/c4KikJbthtWDAlrUE2MuRTDUrSuzmnsq",
+      accounts: ["7a19b0d53f9f119d552522fe15dac7f09b15136fb036bc63660a7dedfd319f72"]
+    }
+  },
   solidity: "0.8.24",
 };
